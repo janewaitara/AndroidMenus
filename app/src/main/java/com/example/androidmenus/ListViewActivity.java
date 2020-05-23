b149721479e0d947274912d18cee4c78720fcf81
janewaitara <janewaitara99@gmail.com>
2020-05-24 01:41:07 +0300

    Created a batch contextual menu and implemented an action on Item clicked

--- app/src/main/java/com/example/androidmenus/ListViewActivity.java
+++ app/src/main/java/com/example/androidmenus/ListViewActivity.java
@@ -3,6 +3,12 @@ package com.example.androidmenus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -20,11 +26,11 @@ public class ListViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
 
-        mListView =(ListView) findViewById(R.id.listViewId);
+        mListView = (ListView) findViewById(R.id.listViewId);
 
         //creating and populating a list of planet names
-        String[] planets = new String[]{"Mercury","Venus","Earth","Mars",
-                "Jupiter","Saturn","Uranus","Neptune" };
+        String[] planets = new String[]{"Mercury", "Venus", "Earth", "Mars",
+                "Jupiter", "Saturn", "Uranus", "Neptune"};
         planetList = new ArrayList<String>();
         planetList.addAll(Arrays.asList(planets));
 
@@ -33,6 +39,65 @@ public class ListViewActivity extends AppCompatActivity {
 
         //setting the arrayAdapter as the ListView Adapter
         mListView.setAdapter(listAdapter);
+
+        //enables selection of many items
+        mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+
+        mListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+            @Override
+            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+
+                //does something when you select or de-select an item
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                //inflate the menu
+                MenuInflater menuInflater = getMenuInflater();
+                menuInflater.inflate(R.menu.batchcontextual_menu, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                //take action when a menu is clicked
+                switch (item.getItemId()){
+                    case R.id.listDelete:
+                        deleteSelectedItem();
+                        mode.finish();
+                        return true;
+                    default:
+                        return false;
+
+                }
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+
+            }
+        });
+
+    }
+
+    private void deleteSelectedItem() {
+
+        // Getting the checked Item from the listView
+        SparseBooleanArray checkedItemPositions = mListView.getCheckedItemPositions();
+        int itemCount = mListView.getCount();
+
+        for (int i = itemCount - 1; i >= 0; i--) {
+            if (checkedItemPositions.get(i)) {
+                listAdapter.remove(planetList.get(i));
+            }
+        }
+        checkedItemPositions.clear();
+        listAdapter.notifyDataSetChanged();
 
     }
 }
--- app/src/main/java/com/example/androidmenus/MainActivity.java
+++ app/src/main/java/com/example/androidmenus/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.view.ActionMode;
 import androidx.appcompat.widget.PopupMenu;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.view.ContextMenu;
@@ -13,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -207,5 +209,11 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
         }
 
 
+    }
+
+    //BATCH CONTEXTUAL
+    public void showListViewActivity(View view) {
+        Intent intent = new Intent(MainActivity.this, ListViewActivity.class);
+        startActivity(intent);
     }
 }
